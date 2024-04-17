d312c70ffcc26d5226761e92061a897a33d6d541
huguitorojas <huguitorojas@gmail.com>
2024-04-16 23:05:09 -0300

    Initial commit

---
+++ src/main/java/com/hugo/web_app/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.hugo.web_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Bienvenido a mi primera aplicacion web";
+    }
+}
